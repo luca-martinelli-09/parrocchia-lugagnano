74cf22d9c4a769d3804c74c15f85171b99d5feca
Luca Martinelli <martinelliluca98@gmail.com>
2022-11-05 14:42:06 +0100

    Aggiunto emporio della solidarietà, tuo figlio vive

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ require_once('config.php');
 
 $conn = getConn();
 
-$getNotifications = $conn->query("SELECT * FROM Avvisi WHERE DataScadenza >= NOW() ORDER BY DataScadenza DESC");
+$getNotifications = $conn->query("SELECT * FROM Avvisi WHERE DataScadenza >= NOW() ORDER BY DataScadenza ASC");
 
 $notifications = array();
 if ($getNotifications && $getNotifications->num_rows > 0) {
@@ -15,12 +15,39 @@ if ($getNotifications && $getNotifications->num_rows > 0) {
   }
 }
 
+function toMonth($m) {
+  $monthConverter = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+  return $monthConverter[intval($m) - 1];
+}
+
+function joinMonths($months) {
+  $firsts = array_slice($months, 0, sizeof($months) - 1);
+  return implode(' e ', [implode(', ', $firsts), $months[sizeof($months) - 1]]);
+}
+
+$notiziari = array();
+foreach (array_slice(array_diff(scandir('./files/notiziari', 1), array('..', '.')), 0, 3) as $file) {
+  $tokens = explode('-', $file);
+  $year = $tokens[0];
+
+  $months = explode('_', $tokens[1]);
+  $months = array_map('toMonth', $months);
+
+  $notiziari[] = [
+    'Mesi' => joinMonths($months),
+    'Titolo' => '"Sui Tuoi Passi", ' . joinMonths($months) . " $year",
+    'Link' => './files/notiziari/' . $file
+  ];
+}
+
 closeConn($conn);
 
 $pug = new Pug();
 
 $pug->displayFile('index.pug', [
   'avvisi' => $notifications,
+  'notiziari' => $notiziari,
   'iscrizioni' => [
     'Login' => LOGIN_ISCRIZIONI,
     'Register' => REGISTER_ISCRIZIONI
---
+++ www/tuofigliovive.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+$pug = new Pug();
+
+$pug->displayFile('tuofigliovive.pug');
