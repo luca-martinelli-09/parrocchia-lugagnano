b75c03b3535ca13ef8c4cef0acdb0fa7d99ebfee
Luca Martinelli <martinelliluca98@gmail.com>
2022-12-27 12:56:33 +0100

    Aggiunti link presepe

--- www/attivita.php
+++ www/attivita.php
@@ -6,7 +6,7 @@ $eventi = array();
 foreach (array_slice(array_diff(scandir('./files/eventi', 1), array('..', '.')), 0, 3) as $file) {
   $tokens = explode('_', str_replace('.jpg', '', $file));
   $day = $tokens[0];
-  $youtubeURI = sizeof($tokens) >= 1 ? $tokens[1] : NULL;
+  $youtubeURI = sizeof($tokens) > 1 ? $tokens[1] : NULL;
 
   $eventi[] = [
     'File' => './files/eventi/' . $file,
--- www/presepe.php
+++ www/presepe.php
@@ -4,8 +4,6 @@ require_once('vendor/autoload.php');
 require_once('config.php');
 require_once('utils.php');
 
-closeConn($conn);
-
 $pug = new Pug();
 
 $pug->displayFile('presepe.pug');
